5081d917fe0cf82ff1e3ec27c86705cd37118a4e
julia-bindi <juliabindi@gmail.com>
2023-10-22 12:26:10 -0300

    fix: remove unixistent references

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,5 @@
 const { hello } = require("./hello.route")
-const { cliente } = require("./cliente.route")
-const { endereco } = require("./endereco.route")
-const { produto } = require("./produto.route")
-const { pedido } = require("./pedido.route")
 
 module.exports = {
     hello,
-    cliente,
-    endereco,
-    produto,
-    pedido,
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,6 @@
 const HelloService = require("./hello.service")
 
-const ClienteService = require("./Cliente")
-const EnderecoService = require("./Endereco")
-const ProdutoService = require("./Produto")
-const PedidosService = require("./Pedido")
 
 module.exports = {
     HelloService,
-    ClienteService,
-    EnderecoService,
-    ProdutoService,
-    PedidosService
 }
