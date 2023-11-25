0057de39dd70af00e4a7c88bfb1a0fe09ba8f8bb
julia-bindi <juliabindi@gmail.com>
2023-11-25 19:33:51 -0300

    feat: create controller to cancel reservation

--- src/controllers/reservation/index.js
+++ src/controllers/reservation/index.js
@@ -1,7 +1,9 @@
 const { create } = require("./create.controller")
 const { check } = require("./check.controller")
+const { cancel } = require("./cancel.controller")
 
 module.exports = {
     create,
     check,
+    cancel,
 }
