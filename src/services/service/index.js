fa2725ec02dd0138efa62b7ad9593582925db32e
julia-bindi <juliabindi@gmail.com>
2023-11-26 11:27:26 -0300

    feat: create search service

--- src/services/service/index.js
+++ src/services/service/index.js
@@ -1,7 +1,9 @@
 const { create } = require("./create.service");
-const { destroy } = require("./delete.service")
+const { destroy } = require("./delete.service");
+const { search } = require("./search.service")
 
 module.exports = {
     create,
     destroy,
+    search,
 }
